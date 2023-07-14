d3bbe9d8724af1382b6fac9804154789493e084a
Oleksii Demennikov <mr.x97@mail.ru>
2023-07-14 19:13:05 +0300

    Added pip installing

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function run(): Promise<void> {
   try {
 		const tags: string = core.getInput('tags');
 		core.info(tags);
+		console.log(execSync('pip install docker-squash').toString());
 		tags.split('/n').forEach(tag => {
 			console.log(execSync(`docker-squash ${tag}`).toString());
 			console.log(execSync(`docker push ${tag}`));
